658a64636c3e4823ce814c8abc1ab61b49ecdc54
Lorenzo Alberton <quipo@php.net>
2004-02-15 22:49:21 +0000

    - update license  PHP 2.02 => 3.0 - add error checking
    git-svn-id: http://svn.php.net/repository/pear/packages/Translation2/trunk@151486 c90b9560-bf6c-de11-be94-00142212c4b1

--- Container/gettext.php
+++ Container/gettext.php
@@ -5,10 +5,10 @@
 // +----------------------------------------------------------------------+
 // | Copyright (c) 1997-2004 The PHP Group                                |
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
+// | This source file is subject to version 3.0 of the PHP license,       |
 // | that is bundled with this package in the file LICENSE, and is        |
 // | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
+// | http://www.php.net/license/3_0.txt.                                  |
 // | If you did not receive a copy of the PHP license and are unable to   |
 // | obtain it through the world-wide-web, please send a note to          |
 // | license@php.net so we can mail you a copy immediately.               |
@@ -24,33 +24,7 @@ require_once 'Translation2'.DIRECTORY_SEPARATOR.'Container.php';
 /**
  * Storage driver for fetching data with gettext
  *
- * =============================
- * langs.ini file format example
- * =============================
- * [en_UK]
- * id = en_UK
- * name = English
- * meta = iso-8859-1
- * error_text = not available in English
- *
- * [it]
- * id = it
- * name = italiano
- * meta = iso-8859-1
- * error_text = non disponibile in italiano
- *
- *
- * ===============================
- * domains.ini file format example
- * ===============================
- * messages = /usr/data/locale
- * errors = /usr/data/locale
- * myApp =  /usr/data/locale
- * myOtherApp = /usr/newData/locale
- *
- *
- *
- *
+ * @see /docs/gettext_readme.txt for an usage example
  * @package  Translation2
  * @version  $Revision$
  */
@@ -99,6 +73,13 @@ class Translation2_Container_gettext extends Translation2_Container
     {
         $this->_setDefaults();
         $this->_domains = @parse_ini_file($this->options['domains_path_file']);
+        if ($this->_domains === false) {
+            return $this->raiseError(
+                'cannot find file '.$this->options['domains_path_file']
+                .' in '.__FILE__.' on line '.__LINE__,
+                TRANSLATION_ERROR_CANNOT_FIND_FILE
+            );
+        }
         foreach ($this->_domains as $domain => $path) {
             bindtextdomain($domain, $path);
         }
@@ -118,7 +99,7 @@ class Translation2_Container_gettext extends Translation2_Container
         $this->options['langs_avail_file']  = 'langs.ini';
         $this->options['domains_path_file'] = 'domains.ini';
         $this->options['default_domain']    = 'messages';
-       //$this->options['path_to_locale']    = './';
+        //$this->options['path_to_locale']  = './';
     }
 
     // }}}
@@ -146,7 +127,7 @@ class Translation2_Container_gettext extends Translation2_Container
     {
         $this->getLangs(); //load available languages, if not loaded yet (ignore return value)
         $this->currentLang = $this->langs[$langID];
-        putenv("LANG=".$this->currentLang['id']);
+        putenv('LANG='.$this->currentLang['id']);
         setlocale(LC_ALL, $this->currentLang['id']);
         return $this->langs[$langID];
     }
@@ -161,7 +142,6 @@ class Translation2_Container_gettext extends Translation2_Container
      */
     function getPage($pageID=null)
     {
-        //if (array_key_exists($pageID, $this->cachedDomains)) {
         if (array_key_exists($this->currentLang['id'], $this->cachedDomains) &&
             array_key_exists($pageID, $this->cachedDomains[$this->currentLang['id']])
         ) {
@@ -172,9 +152,23 @@ class Translation2_Container_gettext extends Translation2_Container
         if (is_null($pageID)) {
             $pageID = $this->options['default_domain'];
         }
-        $domainPath = $this->_domains[$this->currentLang['id']]
+        if (!array_key_exists($pageID, $this->_domains)) {
+            return $this->raiseError(
+                'the domain "'.$pageID.'" was not specified in the .ini file'
+                .' ['.__FILE__.' on line '.__LINE__.']',
+                TRANSLATION_ERROR_DOMAIN_NOT_SET
+            );
+        }
+        $domainPath = $this->_domains[$pageID]
                     . DIRECTORY_SEPARATOR.$this->currentLang['id']
                     . DIRECTORY_SEPARATOR.'LC_MESSAGES';
+        if (!file_exists($domainPath.DIRECTORY_SEPARATOR.$pageID.'.po')) {
+            return $this->raiseError(
+                'cannot find file '.$domainPath.DIRECTORY_SEPARATOR.$pageID.'.po'
+                .' in '.__FILE__.' on line '.__LINE__,
+                TRANSLATION_ERROR_CANNOT_FIND_FILE
+            );
+        }
         $this->cachedDomains[$this->currentLang['id']][$pageID] =
                     Translation2_Utils::po_parser($domainPath, $pageID);
         return $this->cachedDomains[$this->currentLang['id']][$pageID];
@@ -192,7 +186,6 @@ class Translation2_Container_gettext extends Translation2_Container
      */
     function getOne($stringID, $pageID=null, $langID=null)
     {
-        //debug('====== IN GET ONE!!!! ======');
         $langChanged = false;
         if (!is_null($langID) && ($langID != $this->currentLang['id'])) {
             $bkpLang = $this->currentLang['id'];
@@ -224,10 +217,17 @@ class Translation2_Container_gettext extends Translation2_Container
      * @param string $pageID
      * @return string
      */
-    function getStringID($string, $pageID)
+    function getStringID($string, $pageID=null)
     {
         if (empty($pageID)) {
             $pageID = $this->options['default_domain'];
+        }
+        if (!array_key_exists($pageID, $this->_domains)) {
+            return $this->raiseError(
+                'the domain "'.$pageID.'" was not specified in the .ini file'
+                .' ['.__FILE__.' on line '.__LINE__.']',
+                TRANSLATION_ERROR_DOMAIN_NOT_SET
+            );
         }
         $this->getPage($pageID);
 
