bdbd1392732f81c718c9fdc83b73773c8e098e9c
Michael Wallner <mike@php.net>
2004-11-02 16:54:37 +0000

    * simple gettext admin script
    git-svn-id: http://svn.php.net/repository/pear/packages/Translation2/trunk@171975 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ docs/examples/gettext_admin.php
@@ -0,0 +1,54 @@
+<?php
+require_once 'Translation2.php';
+require_once 'Translation2/Admin.php';
+require_once 'I18Nv2/Locale.php';
+
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$options = array(
+    'langs_avail_file'  => 'gettext_langs.ini',
+    'domains_path_file' => 'gettext_domains.ini',
+    'default_domain'    => 'admin'
+);
+
+$tr = &Translation2_Admin::factory('gettext', $options, array('prefetch' => false));
+
+$langs = $tr->getLangs('ids');
+$days  = array();
+$months= array();
+
+$lc = &new I18Nv2_Locale;
+
+foreach ($langs as $lang) {
+    $lc->setLocale($lang);
+    foreach (range(0,6) as $day) {
+        $days[$day][$lang] = $lc->dayName($day);
+    }
+    foreach (range(0,11) as $month) {
+        $months[$month][$lang] = $lc->monthName($month);
+    }
+}
+
+print_r($months);
+
+$tr->storage->begin();
+foreach ($langs as $lang) {
+    foreach (range(0,6) as $day) {
+        $tr->add('day_'. $day, null, $days[$day]);
+    }
+    foreach (range(0,11) as $month) {
+        $tr->add('month_'. $month, null, $months[$month]);
+    }
+}
+$tr->storage->commit();
+
+foreach ($langs as $lang) {
+    foreach (range(0,6) as $day) {
+        echo "$lang day $day: ", $tr->get('day_'. $day, null, $lang), "\n";
+    }
+    foreach (range(0,11) as $month) {
+        echo "$lang month $month: ", $tr->get('month_'. $month, 'admin', $lang), "\n";
+    }
+    echo "\n";
+}
+?>
